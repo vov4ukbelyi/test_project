a09c53fdf9d49a9762cee02859bb65613157c11b
vov4ukbelyi <vov4uk.belyi@gmail.com>
2017-04-30 00:44:29 +0300

    add models and create SU

--- TestTask/product/admin.py
+++ TestTask/product/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug':('name',)}
+    search_fields = ['name']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+    list_filter = ['created_at']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- TestTask/product/models.py
+++ TestTask/product/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=40)
+    description = models.TextField(default='')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=40)
+    description = models.TextField(default='')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
